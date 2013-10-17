76ab1319d66b09fd4c10d7d471e45b25e38c972d
Andy Wermke <andy@dev.next-step-software.com>
2013-10-17 13:00:21 +0200

    Now using a compressed version of localization.min.js.

--- src/views/head.blade.php
+++ src/views/head.blade.php
@@ -1,4 +1,4 @@
 @section('js-localization.head')
-    {{ HTML::script('packages/andywer/js-localization/js/localization.js') }}
+    {{ HTML::script('packages/andywer/js-localization/js/localization.min.js') }}
     <script type="text/javascript" src="{{ action('JsLocalizationController@createJsMessages') }}"></script>
 @stop
