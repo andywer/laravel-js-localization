f05cc942972d56d3f97e2e2659057c6db3daeaac
Herman <sintemaa@hotmail.com>
2014-04-27 23:10:51 +0200

    Set Content-Type application/javascript for localization.js

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,7 @@ Route::get('/js-localization/localization.js', function()
 {
     $response = new StaticFileResponse( __DIR__."/../public/js/localization.min.js" );
     $response->setPublic();
+    $response->header('Content-Type', 'application/javascript');
 
     return $response;
 });
