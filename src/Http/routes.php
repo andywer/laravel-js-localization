dafe2814e52e2f6631b4b7750f4a3afe772a34de
Andy Wermke <andy@dev.next-step-software.com>
2015-04-10 09:29:11 +0200

    Commit files forgot to add to last commit

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -8,7 +8,7 @@ Route::group([ 'namespace' => '\JsLocalization\Http\Controllers' ], function()
 
     Route::get('/js-localization/localization.js', function()
     {
-        $response = new StaticFileResponse( __DIR__."/../../../public/js/localization.min.js" );
+        $response = new StaticFileResponse( __DIR__."/../../public/js/localization.min.js" );
         $response->setPublic();
         $response->header('Content-Type', 'application/javascript');
 
