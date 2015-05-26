751f8c43553a8522a711a853bc2f91528d9881bd
Michael Scharl <ms@campaigning-bureau.com>
2015-05-26 18:09:47 +0200

    add tempalte section that uses the all.js-File

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,3 +2,7 @@
     <script type="text/javascript" src="{{ url('/js-localization/localization.js') }}"></script>
     <script type="text/javascript" src="{{ url('/js-localization/messages') }}"></script>
 @stop
+
+@section('js-localization.head.all_in_one')
+    <script type="text/javascript" src="{{ url('/js-localization/all.js') }}"></script>
+@stop
