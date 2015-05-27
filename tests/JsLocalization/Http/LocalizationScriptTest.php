a93d4493ed49a190a18419e0ac8ed85d6ca9186d
Andy Wermke <andy@dev.next-step-software.com>
2015-05-27 17:20:12 +0200

    Add test for all.js request

--- tests/JsLocalization/Http/LocalizationScriptTest.php
+++ tests/JsLocalization/Http/LocalizationScriptTest.php
@@ -7,8 +7,32 @@ class LocalizationScriptTest extends TestCase
         $response = $this->call('GET', '/js-localization/localization.js');
 
         $this->assertTrue($response->isOk());
-
         $content = $response->getContent();
+        
+        // Test for JS content
+        
         $this->assertRegExp('/^\(function\(.*\);/', $content);
+    }
+    
+    public function testScriptAndTranslationCombinedRetrieval()
+    {
+        $response = $this->call('GET', '/js-localization/all.js');
+
+        $this->assertTrue($response->isOk());
+        $content = $response->getContent();
+
+        // Test for JS content
+
+        $this->assertRegExp('/^\(function\(.*\);/', $content);
+
+        // Test for Lang.setLocale()
+
+        $locale = \Illuminate\Support\Facades\Lang::locale();
+        $this->assertRegExp('/Lang\.setLocale\("'.$locale.'"\);/', $content);
+        
+        // Test for Land.addMessages()
+
+        $addMessagesRegex = '/Lang\.addMessages\( (\{.*?\}) \);/x';
+        $this->assertRegExp($addMessagesRegex, $content);
     }
 }
