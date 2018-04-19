2a76a2122700eca754ee539a332cec5280bc55df
Alexander Karlstad <alexander.karlstad@gmail.com>
2018-04-19 12:41:58 +0200

    Add a more automatic @section to head.blade.php that can be used when exporting translations - @yield('js-localization.head.exported')

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -17,3 +17,24 @@
         Lang.setLocale("{{ App::getLocale() }}");
     </script>
 @stop
+
+@section('js-localization.head.exported')
+    @if (file_exists(config('js-localization.storage_path') . '/js-localization.min.js'))
+    <script src="{{ url('/vendor/js-localization/js-localization.min.js') }}"></script>
+    @else
+    <script src="{{ url('/js-localization/localization.js') }}"></script>
+    @endif
+    
+    @if (Config::get('js-localization.config'))
+    <script src="{{ url('/vendor/js-localization/config.js') }}"></script>
+    @endif
+
+    @if (Config::get('js-localization.split_export_files') && file_exists(config('js-localization.storage_path') . '/lang-' . App::getLocale() . '.js'))
+    <script src="{{ url('/vendor/js-localization/lang-' . App::getLocale() . '.js') }}"></script>
+    @else
+    <script src="{{ url('/vendor/js-localization/messages.js') }}"></script>
+    @endif
+    <script>
+        Lang.setLocale("{{ App::getLocale() }}");
+    </script>
+@stop
